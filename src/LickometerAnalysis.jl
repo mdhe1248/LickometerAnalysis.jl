8f34c6a8539fa7c5964de3a587ad637c748c6b86
Donghoon Lee <mdhe1248@gmail.com>
2022-03-30 11:21:22 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/LickometerAnalysis.jl
@@ -0,0 +1,5 @@
+module LickometerAnalysis
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LickometerAnalysis
+using Test
+
+@testset "LickometerAnalysis.jl" begin
+    # Write your tests here.
+end
